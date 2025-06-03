a056a3bbf31be40d85a952b17e105c9dabeee891
kevin hansen <kevin.hansen014@gmail.com>
2025-06-03 20:07:03 +0000

    Updated file extension

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import "./ProductList.css";
-import CartItem from "./CartItem";
-import { addItem } from "./CartSlice";
+import CartItem from "./CartItem.jsx";
+import { addItem } from "./CartSlice.jsx";
 function ProductList({ onHomeClick }) {
   const [showCart, setShowCart] = useState(false);
   const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
